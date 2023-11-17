c869892c5ee2e6c861fd63f98619f542ea346fff
bindumounica <74135079+bindumounica@users.noreply.github.com>
2023-11-17 16:46:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "terraform_rg"
     storage_account_name = "terraformsg"
-    container_name       = "terraform"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
